ca351d7a3082230a154eca742a36819fcacb0018
Luca <lucagough7@gmail.com>
2025-06-20 17:39:56 +0100

    stuff

--- mmdet3d/datasets/custom_visual_dataset.py
+++ mmdet3d/datasets/custom_visual_dataset.py
@@ -1,41 +1,96 @@
+import os.path as osp
+from typing import Callable, List, Optional, Union
 
-
-import mmengine
+import numpy as np
 
 from mmdet3d.registry import DATASETS
+from mmdet3d.structures import DepthInstance3DBoxes
 from .det3d_dataset import Det3DDataset
 
 
 @DATASETS.register_module()
 class CustomVisualDataset(Det3DDataset):
 
-    # replace with all the classes in customized pkl info file
     METAINFO = {
         'classes': ('Cube')
     }
 
-    def parse_ann_info(self, info):
+    def __init__(self,
+                 data_root: str,
+                 ann_file: str,
+                 metainfo: Optional[dict] = None,
+                 data_prefix: dict = dict(img='image'),
+                 pipeline: List[Union[dict, Callable]] = [],
+                 default_cam_key: str = 'CAM0',
+                 modality: dict = dict(use_camera=True, use_lidar=False),
+                 box_type_3d: str = 'Camera',
+                 filter_empty_gt: bool = True,
+                 test_mode: bool = False,
+                 **kwargs) -> None:
+        super().__init__(
+            data_root=data_root,
+            ann_file=ann_file,
+            metainfo=metainfo,
+            data_prefix=data_prefix,
+            pipeline=pipeline,
+            default_cam_key=default_cam_key,
+            modality=modality,
+            box_type_3d=box_type_3d,
+            filter_empty_gt=filter_empty_gt,
+            test_mode=test_mode,
+            **kwargs)
+        
+        assert 'use_camera' in self.modality
+        assert self.modality['use_camera'] or self.modality['use_lidar']
+
+    def parse_data_info(self, info: dict) -> dict:
+        """Process the raw data info.
+
+        Convert all relative path of needed modality data file to
+        the absolute path. And process
+        the `instances` field to `ann_info` in training stage.
+
+        Args:
+            info (dict): Raw info dict.
+
+        Returns:
+            dict: Has `ann_info` in training stage. And
+            all path has been converted to absolute path.
+        """
+
+        if self.modality['use_camera']:
+            for cam_id, img_info in info['images'].items():
+                if 'img_path' in img_info:
+                    img_info['img_path'] = osp.join(self.data_prefix.get('img', ''), img_info['img_path'])
+            if self.default_cam_key is not None:
+                info['img_path'] = info['images'][self.default_cam_key]['img_path']
+                info['cam2img'] = np.array(info['images'][self.default_cam_key]['cam2img'], dtype=np.float32)
+
+        if not self.test_mode:
+            info['ann_info'] = self.parse_ann_info(info)
+        if self.test_mode and self.load_eval_anns:
+            info['eval_ann_info'] = self.parse_ann_info(info)
+
+        return info
+
+    def parse_ann_info(self, info: dict) -> dict:
         """Process the `instances` in data info to `ann_info`.
 
         Args:
-            info (dict): Data information of single data sample.
+            info (dict): Info dict.
 
         Returns:
-            dict: Annotation information consists of the following keys:
-
-                - gt_bboxes_3d (:obj:`LiDARInstance3DBoxes`):
-                  3D ground truth bboxes.
-                - gt_labels_3d (np.ndarray): Labels of ground truths.
+            dict: Processed `ann_info`
         """
         ann_info = super().parse_ann_info(info)
+        # process data without any annotations
         if ann_info is None:
             ann_info = dict()
-            # empty instance
-            ann_info['gt_bboxes_3d'] = np.zeros((0, 7), dtype=np.float32)
-            ann_info['gt_labels_3d'] = np.zeros(0, dtype=np.int64)
+            ann_info['gt_bboxes_3d'] = np.zeros((0, 6), dtype=np.float32)
+            ann_info['gt_labels_3d'] = np.zeros((0, ), dtype=np.int64)
+        # to target box structure
+        ann_info['gt_bboxes_3d'] = DepthInstance3DBoxes(
+            ann_info['gt_bboxes_3d'],
+            origin=(0.5, 0.5, 0.5)).convert_to(self.box_mode_3d)
 
-        # filter the gt classes not used in training
-        ann_info = self._remove_dontcare(ann_info)
-        gt_bboxes_3d = CameraInstance3DBoxes(ann_info['gt_bboxes_3d'])
-        ann_info['gt_bboxes_3d'] = gt_bboxes_3d
         return ann_info
