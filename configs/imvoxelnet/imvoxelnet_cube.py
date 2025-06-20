91dabd7d87476bc8e351f9fe8ace380d098c099e
Luca <lucagough7@gmail.com>
2025-06-20 17:46:19 +0100

    stuff

--- configs/imvoxelnet/imvoxelnet_cube.py
+++ configs/imvoxelnet/imvoxelnet_cube.py
@@ -48,7 +48,7 @@ model = dict(
     train_cfg=dict(),
     test_cfg=dict(nms_pre=1000, iou_thr=.25, score_thr=.01))
 
-dataset_type = 'CustomVisualDataset'
+dataset_type = 'SUNRGBDDataset'
 data_root = 'data/data/custom'
 class_names = [
     'Cube'
