69945fa5fac3777a3f0193ba96fca46acdd320d0
Luca <lucagough7@gmail.com>
2025-06-22 12:37:34 +0100

    stuff

--- tools/dataset_converters/custom_visual_data_utils.py
+++ tools/dataset_converters/custom_visual_data_utils.py
@@ -13,7 +13,7 @@ class CustomVisualInstance(object):
         data = line.split(' ')
         data[:-1] = [float(x) for x in data[:-1]]
         self.classname = data[-1]
-        self.centroid = np.array([data[0], data[1], data[2]+15])
+        self.centroid = np.array([data[0], data[1], data[2]+2])
         self.size = np.array([data[3], data[4], data[5]])
         self.yaw = np.array([data[6]])
         self.length = data[3]
