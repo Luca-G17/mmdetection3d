b2625522ee7e791477931495429d8588c4cb482a
Luca <lucagough7@gmail.com>
2025-06-23 10:39:16 +0100

    stuff

--- mmdet3d/datasets/custom_visual_dataset.py
+++ mmdet3d/datasets/custom_visual_dataset.py
@@ -92,6 +92,6 @@ class CustomVisualDataset(Det3DDataset):
             ann_info['gt_bboxes_3d'] = np.zeros((0, 6), dtype=np.float32)
             ann_info['gt_labels_3d'] = np.zeros((0, ), dtype=np.int64)
         # to target box structure
-        ann_info['gt_bboxes_3d'] = CameraInstance3DBoxes(ann_info['gt_bboxes_3d']).convert_to(self.box_mode_3d)
+        ann_info['gt_bboxes_3d'] = CameraInstance3DBoxes(ann_info['gt_bboxes_3d']).convert_to(self.box_mode_3d).flip()
 
         return ann_info
