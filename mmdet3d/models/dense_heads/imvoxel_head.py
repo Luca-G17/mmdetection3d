e0f9ce76f2f7e76f7ad1723eabef639eec6d7786
Luca <lucagough7@gmail.com>
2025-06-27 11:35:37 +0100

    updating model

--- mmdet3d/models/dense_heads/imvoxel_head.py
+++ mmdet3d/models/dense_heads/imvoxel_head.py
@@ -177,7 +177,7 @@ class ImVoxelHead(BaseModule):
 
         valid_pred = x[-1]
         outs = self(x[:-1])
-
+        print(outs.shape)
         loss_inputs = outs + (valid_pred, batch_gt_instances_3d,
                               batch_input_metas, batch_gt_instances_ignore)
         losses = self.loss_by_feat(*loss_inputs)
