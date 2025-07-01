8467918924a3364301230943b2a673b01f657892
Luca <lucagough7@gmail.com>
2025-07-01 10:15:52 +0100

    added stuff

--- configs/imvoxelnet/set_config_datapath.py
+++ configs/imvoxelnet/set_config_datapath.py
@@ -3,7 +3,7 @@ import sys
 def main():
     config_path = sys.argv[1]
     data_root = sys.argv[2]
-    with open(config_path, "w") as cfg:
+    with open(config_path, "a") as cfg:
         data = cfg.readlines()
         data[0] = f"data_root = '{data_root}'"
         print(data_root)
