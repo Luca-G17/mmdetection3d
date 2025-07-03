0ff017aa6ec5192d3c49fac1e905c62255cdeb97
Luca <lucagough7@gmail.com>
2025-07-03 12:32:27 +0100

    added stuff

--- mmdet3d/models/detectors/imvoxelnet.py
+++ mmdet3d/models/detectors/imvoxelnet.py
@@ -88,7 +88,6 @@ class ImVoxelNet(Base3DDetector):
         imgs = torch.stack(imgs, dim=0)          # Tensor: [B, 2, 4, 480, 640]
         img = imgs[:, 0]
 
-        img = print(img.shape)
         batch_img_metas = [
             data_samples.metainfo for data_samples in batch_data_samples
         ]
