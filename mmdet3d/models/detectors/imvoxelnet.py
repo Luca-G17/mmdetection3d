3db61117677a7fd38a869e4fb2f95f0f3db89c3d
Luca <lucagough7@gmail.com>
2025-07-03 13:38:38 +0100

    added stuff

--- mmdet3d/models/detectors/imvoxelnet.py
+++ mmdet3d/models/detectors/imvoxelnet.py
@@ -97,10 +97,12 @@ class ImVoxelNet(Base3DDetector):
                                                    device=img.device)[0][:, :3]
         volumes, valid_preds = [], []
         for feature, img_meta in zip(x, batch_img_metas):
-            print(img_meta)
-            img_scale_factor = (
-                points.new_tensor(img_meta['scale_factor'][:2])
-                if 'scale_factor' in img_meta.keys() else 1)
+            scale = img_meta.get('scale_factor', 1)
+            if isinstance(scale, (list, tuple)):
+                img_scale_factor = points.new_tensor(scale[:2])
+            else:
+                img_scale_factor = points.new_tensor([scale, scale])
+                
             img_flip = img_meta['flip'] if 'flip' in img_meta.keys() else False
             img_crop_offset = (
                 points.new_tensor(img_meta['img_crop_offset'])
