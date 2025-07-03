7065c0611d0e541a4b5fb7ae286c2ff0c510dc53
Luca <lucagough7@gmail.com>
2025-07-03 13:32:47 +0100

    added stuff

--- mmdet3d/models/detectors/imvoxelnet.py
+++ mmdet3d/models/detectors/imvoxelnet.py
@@ -83,12 +83,11 @@ class ImVoxelNet(Base3DDetector):
             - torch.Tensor: Features of shape (N, C_out, N_x, N_y, N_z).
             - torch.Tensor: Valid mask of shape (N, 1, N_x, N_y, N_z).
         """
-        print(batch_inputs_dict.keys())
         imgs = batch_inputs_dict['img']
         imgs = torch.stack(imgs, dim=0)          # Tensor: [B, 2, 4, 480, 640]
         img = imgs[:, 0]
         img = img[:, :3].float()
-
+        print(img.shape)
         batch_img_metas = [
             data_samples.metainfo for data_samples in batch_data_samples
         ]
