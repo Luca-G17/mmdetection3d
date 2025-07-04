c2a0764400aa8eb953234153c58e51da04d2b3ce
Luca <lucagough7@gmail.com>
2025-07-04 15:49:55 +0100

    added stuff

--- configs/imvoxelnet/imvoxelnet_cube_mv.py
+++ configs/imvoxelnet/imvoxelnet_cube_mv.py
@@ -5,7 +5,7 @@ _base_ = [
 ]
 prior_generator = dict(
     type='AlignedAnchor3DRangeGenerator',
-    ranges=[[-5, -5, -5, 5, 5, 5]],
+    ranges=[[-20, -20, -20, 20, 20, 20]],
     rotations=[.0])
 model = dict(
     type='ImVoxelNet',
