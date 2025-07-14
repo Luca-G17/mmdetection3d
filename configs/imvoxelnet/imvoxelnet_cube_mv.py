fbd1722359370885da6e17ac058c98bef39f35ce
Luca <lucagough7@gmail.com>
2025-07-14 16:57:46 +0100

    increasing batch size

--- configs/imvoxelnet/imvoxelnet_cube_mv.py
+++ configs/imvoxelnet/imvoxelnet_cube_mv.py
@@ -42,7 +42,7 @@ model = dict(
     n_voxels=[40, 40, 40],
     coord_type='CAMERA',
     train_cfg=dict(),
-    test_cfg=dict(nms_pre=10000, iou_thr=.25, score_thr=.001))
+    test_cfg=dict(nms_pre=10000, iou_thr=.1, score_thr=.001))
 
 dataset_type = 'CustomVisualDataset'
 class_names = [
