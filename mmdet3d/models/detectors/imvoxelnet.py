ea47fd1eacb292302efa4d4d74421f61a422d389
Luca <lucagough7@gmail.com>
2025-07-21 16:08:55 +0100

    updated config

--- mmdet3d/models/detectors/imvoxelnet.py
+++ mmdet3d/models/detectors/imvoxelnet.py
@@ -218,7 +218,7 @@ class ImVoxelNet(Base3DDetector):
 
         img_filepath = batch_img_metas[0]['img_path'][0]
         dataset_path = f"{img_filepath.split('images')[0]}/pc_vis/"
-        pc_filepath = f'{img_filepath.split('/')[-1].split(".")[0]}.ply'
+        pc_filepath = f"{img_filepath.split('/')[-1].split('.')[0]}.ply"
         os.makedirs(dataset_path, exist_ok=True) 
         self.save_pointcloud_from_voxels(
             fused_volumes[0],
