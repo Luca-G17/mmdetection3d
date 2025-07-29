c3cd93b74c55ecea19f41135f0d73c73fe84ee0e
Luca <lucagough7@gmail.com>
2025-07-29 16:53:34 +0100

    updated report

--- mmdet3d/visualization/local_visualizer.py
+++ mmdet3d/visualization/local_visualizer.py
@@ -1023,6 +1023,7 @@ class Det3DLocalVisualizer(DetLocalVisualizer):
                 pred_instances_3d = data_sample.pred_instances_3d
                 # .cpu can not be used for BaseInstance3DBoxes
                 # so we need to use .to('cpu')
+                print(pred_instances_3d)
                 pred_instances_3d = pred_instances_3d[pred_instances_3d.scores_3d > 0.1].to('cpu')
 
 
